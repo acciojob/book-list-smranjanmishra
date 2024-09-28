435e66d7c7701acb0a9c1c4660e1ef9cdee3d988
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-09-28 22:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-//your JS code here. If required.
-const form = document.getElementById("bookForm") ;
-function addBook() {
- var title = document.getElementById('title').value;
- var author = document.getElementById('author').value;
- var isbn = document.getElementById('isbn').value;
- if (title && author && isbn) {
-     var tableBody = document.getElementById('book-list');
-     var newRow = tableBody.insertRow();
-     
-     var cell1 = newRow.insertCell(0);
-     var cell2 = newRow.insertCell(1);
-     var cell3 = newRow.insertCell(2);
-     var cell4 = newRow.insertCell(3);
-	 
-     cell1.innerHTML = title;
-     cell2.innerHTML = author;
-     cell3.innerHTML = isbn;
-     cell4.innerHTML = '<span class="delete" onclick="deleteRow(this)">X</span>';
-     
-	 form.reset() ;
- }
-}
 
- function deleteRow(element) {
-     var row = element.parentNode.parentNode;
-     row.parentNode.removeChild(row);
- }
+//your JS code here. If required.
+
+document.getElementById("submit").addEventListener("click", function(event) {
+      event.preventDefault();
+      addBookToList();
+    });
+let tbody=document.getElementById("book-list");
+function addBookToList(){
+	let row=tbody.insertRow();
+	tbody.append(row);
+	let td1=row.insertCell(0);
+	td1.innerText=document.getElementById("title").value;
+	let td2=row.insertCell(1);
+	td2.innerText=document.getElementById("author").value;
+	let td3=row.insertCell(2);
+	td3.innerText=document.getElementById("isbn").value;
+	let td4=row.insertCell(3);
+	let newBtn = document.createElement('button');
+	newBtn.className="delete";
+    newBtn.innerText = 'X';
+	row.append(td1);
+	row.append(td2);
+	row.append(td3);
+	row.append(td4);
+	td4.append(newBtn);
+ 
+	var deleteButtons = document.getElementsByClassName("delete");
+      for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function(event) {
+          event.target.parentNode.parentNode.remove();
+        });
+      }	
+}
